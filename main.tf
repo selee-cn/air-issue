f8b525fa570fce9ce2e62a0f763edcf70ed135bf
selee-cn <167044426+selee-cn@users.noreply.github.com>
2024-08-21 17:41:58 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_security_group" "sg" {
+  name        = "sg-example"
+  vpc_id      = "vpc-054f7b1ecfa344769"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "sg-rule" {
+  security_group_id = aws_security_group.sg.id
+  referenced_security_group_id = "022499047528/sg-01e480a6adb7ce9ad"
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
