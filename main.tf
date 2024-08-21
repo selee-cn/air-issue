87021fc3b21ab9676ee3dcf987c16d11c1d62bd6
selee-cn <167044426+selee-cn@users.noreply.github.com>
2024-08-21 18:49:16 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,14 +6,13 @@ provider "aws" {
 resource "aws_security_group" "sg" {
   name        = "example-security-group"
   vpc_id      = "vpc-381c8153"
+
+  ingress {
+    from_port                = 80
+    to_port                  = 80
+    protocol                 = "tcp"
+    security_groups          = ["sg-01e480a6adb7ce9ad"]  # 타계정의 Security Group ID
+    description              = "Allow traffic from the referenced security group in another account"
+  }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "sg-rule" {
-  security_group_id = aws_security_group.sg.id
-  #referenced_security_group_id = "022499047528/sg-01e480a6adb7ce9ad"
-
-  from_port   = 80
-  ip_protocol = "tcp"
-  security_groups = ["sg-01e480a6adb7ce9ad"] 
-  to_port     = 80
-}
