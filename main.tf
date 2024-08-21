6b8a29598b5a1ae7e0c68bf2e33eb2929ddd42d1
selee-cn <167044426+selee-cn@users.noreply.github.com>
2024-08-21 18:39:58 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "sg-rule" {
   security_group_id = aws_security_group.sg.id
-  referenced_security_group_id = "022499047528/sg-01e480a6adb7ce9ad"
+  referenced_security_group_id = "022499047528\/sg-01e480a6adb7ce9ad"
 
   from_port   = 80
   ip_protocol = "tcp"
