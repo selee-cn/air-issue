23e6012ff6b7797bee2f91f5c0168c9d18bec082
selee-cn <167044426+selee-cn@users.noreply.github.com>
2024-08-22 09:40:06 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "sg" {
     from_port                = 80
     to_port                  = 80
     protocol                 = "tcp"
-    security_groups          = ["sg-01e480a6adb7ce9ad"]  # 타계정의 Security Group ID
+    security_groups          = ["sg-032fa204f8079f9d0"]  # 타계정의 Security Group ID
     description              = "Allow traffic from the referenced security group in another account"
   }
 }
